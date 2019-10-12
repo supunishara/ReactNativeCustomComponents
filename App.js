8e11d56af691f58afff795f3955a85926cfb4930
Supun Ishara <supun.ishara@i4tradies.com>
2019-10-12 22:18:48 +0530

    initial Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import Route from './Src/Routes';
+
+const App = () => <Route />;
+
+export default App;
---
+++ Src/Routes.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+import Home from './Home';
+import Settings from './Settings';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// const TabNavigator = createBottomTabNavigator({
+//   Home: Home,
+//   Settings: Settings,
+// });
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: Home,
+    Settings: Settings,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused, horizontal, tintColor}) => {
+        const {routeName} = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+          // Sometimes we want to add badges to some icons.
+          // You can check the implementation below.
+          // IconComponent = HomeIconWithBadge;
+        } else if (routeName === 'Settings') {
+          iconName = `ios-options`;
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  },
+);
+
+const Route = createStackNavigator({
+  Home: {
+    screen: Home,
+  },
+});
+
+export default createAppContainer(TabNavigator);
