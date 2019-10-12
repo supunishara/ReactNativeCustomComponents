b9963c9f56c00d693ccab60cea4249ef99742f4e
Supun Ishara <supun.ishara@i4tradies.com>
2019-10-12 22:54:58 +0530

    Material bottom and MaterialTop Navigation added

--- Src/Routes.js
+++ Src/Routes.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import {StyleSheet, Text} from 'react-native';
+
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {
+  createBottomTabNavigator,
+  createMaterialTopTabNavigator,
+} from 'react-navigation-tabs';
 
 import Home from './Home';
 import Settings from './Settings';
+
+import One from './One';
+import Two from './Two';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
@@ -13,10 +21,57 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 //   Settings: Settings,
 // });
 
+const UpperScreens = {
+  One: One,
+  Two: Two,
+};
+
+const UpperNavigator = createMaterialTopTabNavigator(UpperScreens, {
+  navigationOptions: ({navigation, screenProps}) => ({
+    header: null,
+    headerMode: 'none',
+    // tabBarVisible: true,
+    tabBarLabel: ({focused}) => {
+      const {routeName, index} = navigation.state;
+      switch (routeName) {
+      }
+      return <Text>routeName</Text>;
+    },
+  }),
+  animationEnabled: false,
+  swipeEnabled: true,
+  lazy: true,
+  tabBarOptions: {
+    scrollEnabled: true,
+    activeTintColor: 'rgb(12,157,197)',
+    inactiveTintColor: 'black',
+    indicatorStyle: {
+      backgroundColor: 'rgb(102,134,205)',
+      height: 0,
+    },
+    labelStyle: {
+      fontSize: 14 * 1, //ratio,
+      color: 'tomato',
+      backgroundColor: '#009432',
+    },
+    tabStyle: {
+      height: 48,
+      width: 110,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    style: {
+      backgroundColor: 'white',
+    },
+    statusBarStyle: 'light-content',
+  },
+});
+
 const TabNavigator = createBottomTabNavigator(
   {
     Home: Home,
     Settings: Settings,
+    UpperNavigator: UpperNavigator,
   },
   {
     defaultNavigationOptions: ({navigation}) => ({
---
+++ Src/Two.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, Dimensions} from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+const Width = Dimensions.get('window').width;
+const Height = Dimensions.get('window').height;
+
+export default class Two extends Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return <View style={styles.Container}></View>;
+  }
+}
+
+const styles = StyleSheet.create({
+  Container: {
+    width: Width,
+    height: Height,
+    backgroundColor: 'blue',
+  },
+});
